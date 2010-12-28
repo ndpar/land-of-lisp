50f51279e30e56bb71bfcafcfb833166a6d7c324
Andrey Paramonov <git@ndpar.com>
2010-12-27 19:32:57 -0500

    Chapter 20: final version of DoD game
    - Increased number of players and dice
    - Rolled dice
    - Changed reinforcement rules
    - Reduced AI tree depth
    - Removed alpha-bata pruning
    - add-new-dice is not tail-recursive

--- dice-of-doom.lisp
+++ dice-of-doom.lisp
@@ -6,8 +6,8 @@
 
 (load "lazy")
 
-(defparameter *num-players* 2)
-(defparameter *max-dice* 3)
+(defparameter *num-players* 4)
+(defparameter *max-dice* 5)
 (defparameter *board-size* 5)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
 
@@ -65,6 +65,10 @@
                                                                  (game-tree (board-attack board cur-player src dst (dice src))
                                                                             cur-player
                                                                             (+ spare-dice (dice dst))
+                                                                            nil)
+                                                                 (game-tree (board-attack-fail board cur-player src dst (dice src))
+                                                                            cur-player
+                                                                            (+ spare-dice (dice dst))
                                                                             nil))))
                                           (lazy-nil)))
                                     (make-lazy (neighbors src)))
@@ -90,21 +94,70 @@
                                    ((eq pos dst) (list player (1- dice)))
                                    (t hex)))))
 
-; Tail-recursion example
+(defun board-attack-fail (board player src dst dice)
+  (board-array (loop for pos from 0
+                     for hex across board
+                     collect (if (eq pos src)
+                                 (list player 1)
+                                 hex))))
+
+(defun roll-dice (dice-num)
+  (let ((total (loop repeat dice-num
+                     sum (1+ (random 6)))))
+    (fresh-line)
+    (format t "On ~a dice rolled ~a. " dice-num total)
+    total))
+
+(defun roll-against (src-dice dst-dice)
+  (> (roll-dice src-dice) (roll-dice dst-dice)))
+
+(defun pick-chance-branch (board move)
+  (labels ((dice (pos)
+             (cadr (aref board pos))))
+    (let ((path (car move)))
+      (if (or (null path) (roll-against (dice (car path))
+                                        (dice (cadr path))))
+          (cadr move)
+          (caddr move)))))
+
+(defun get-connected (board player pos)
+  (labels ((check-pos (pos visited)
+             (if (and (eq (car (aref board pos)) player)
+                      (not (member pos visited)))
+                 (check-neighbors (neighbors pos) (cons pos visited))
+                 visited))
+           (check-neighbors (lst visited)
+             (if lst
+                 (check-neighbors (cdr lst) (check-pos (car lst) visited))
+                 visited)))
+    (check-pos pos '())))
+
+(defun largest-cluster-size (board player)
+  (labels ((f (pos visited best)
+              (if (< pos *board-hexnum*)
+                  (if (and (eq (car (aref board pos)) player)
+                           (not (member pos visited)))
+                      (let* ((cluster (get-connected board player pos))
+                             (size (length cluster)))
+                        (if (> size best)
+                            (f (1+ pos) (append cluster visited) size)
+                            (f (1+ pos) (append cluster visited) best)))
+                      (f (1+ pos) visited best))
+                  best)))
+    (f 0 '() 0)))
+
 (defun add-new-dice (board player spare-dice)
-  (labels ((f (lst n acc)
-             (cond ((zerop n) (append (reverse acc) lst))
-                   ((null lst) (reverse acc))
-                   (t (let ((cur-player (caar lst))
-                            (cur-dice (cadar lst)))
-                        (if (and (eq cur-player player) (< cur-dice *max-dice*))
-                            (f (cdr lst)
-                               (1- n)
-                               (cons (list cur-player (1+ cur-dice)) acc))
-                            (f (cdr lst)
-                               n
-                               (cons (car lst) acc))))))))
-    (board-array (f (coerce board 'list) spare-dice ()))))
+  (labels ((f (lst n)
+              (cond ((zerop n) lst)
+                    ((null lst) nil)
+                    (t (let ((cur-player (caar lst))
+                             (cur-dice (cadar lst)))
+                         (if (and (eq cur-player player) (< cur-dice *max-dice*))
+                             (cons (list cur-player (1+ cur-dice))
+                                   (f (cdr lst) (1- n)))
+                             (cons (car lst) (f (cdr lst) n))))))))
+    (board-array (f (coerce board 'list)
+                        (largest-cluster-size board player)))))
 
 (defun play-vs-human (tree)
   (print-info tree)
@@ -133,7 +186,7 @@
                  (print-moves (lazy-cdr moves) (1+ n)))))
       (print-moves moves 1))
       (fresh-line)
-      (cadr (lazy-nth (1- (read)) moves))))
+      (pick-chance-branch (cadr (lazy-nth (1- (read)) moves)))))
 
 (defun winners (board)
   (let* ((tally (loop for hex across board
@@ -176,43 +229,31 @@
                (when (and (not (eq player nplayer)) (> ndice dice))
                  (return t))))))
 
-(defun ab-get-ratings-max (tree player upper-limit lower-limit)
-  (labels ((f (moves lower-limit)
-              (unless (lazy-null moves)
-                (let ((x (ab-rate-position (cadr (lazy-car moves))
-                                           player
-                                           upper-limit
-                                           lower-limit)))
-                  (if (>= x upper-limit)
-                      (list x)
-                      (cons x (f (lazy-cdr moves) (max x lower-limit))))))))
-    (f (caddr tree) lower-limit)))
+(defun rate-position (tree player)
+   (let ((moves (caddr tree)))
+     (if (not (lazy-null moves))
+         (apply (if (eq (car tree) player)
+                    #'max
+                    #'min)
+                (get-ratings tree player))
+         (score-board (cadr tree) player))))
 
-(defun ab-get-ratings-min (tree player upper-limit lower-limit)
-  (labels ((f (moves upper-limit)
-              (unless (lazy-null moves)
-                (let ((x (ab-rate-position (cadr (lazy-car moves))
-                                           player
-                                           upper-limit
-                                           lower-limit)))
-                  (if (<= x lower-limit)
-                      (list x)
-                      (cons x (f (lazy-cdr moves) (min x upper-limit))))))))
-    (f (caddr tree) upper-limit)))
-
-(defun ab-rate-position (tree player upper-limit lower-limit)
-  (let ((moves (caddr tree)))
-    (if (not (lazy-null moves))
-        (if (eq (car tree) player)
-            (apply #'max (ab-get-ratings-max tree
-                                             player
-                                             upper-limit
-                                             lower-limit))
-            (apply #'min (ab-get-ratings-min tree
-                                             player
-                                             upper-limit
-                                             lower-limit)))
-        (score-board (cadr tree) player))))
+(defun get-ratings (tree player)
+  (let ((board (cadr tree)))
+    (labels ((dice (pos)
+               (cadr (aref board pos))))
+      (take-all (lazy-mapcar
+                  (lambda (move)
+                    (let ((path (car move)))
+                      (if path
+                          (let* ((src (car path))
+                                 (dst (cadr path))
+                                 (odds (aref (aref *dice-odds* (1- (dice dst)))
+                                             (- (dice src) 2))))
+                            (+ (* odds (rate-position (cadr move) player))
+                               (* (- 1 odds) (rate-position (caddr move) player))))
+                          (rate-position (cadr move) player))))
+                  (caddr tree))))))
 
 (defun limit-tree-depth (tree depth)
   (list (car tree)
@@ -220,24 +261,30 @@
         (if (zerop depth)
             (lazy-nil)
             (lazy-mapcar (lambda (move)
-                           (list (car move)
-                                 (limit-tree-depth (cadr move) (1- depth))))
+                           (cons (car move)
+                                 (mapcar (lambda (x)
+                                           (limit-tree-depth x (1- depth)))
+                                         (cdr move))))
                          (caddr tree)))))
 
-(defparameter *ai-level* 4)
+(defparameter *ai-level* 2)
 
 (defun handle-computer (tree)
-  (let ((ratings (ab-get-ratings-max (limit-tree-depth tree *ai-level*)
-                                     (car tree)
-                                     most-positive-fixnum
-                                     most-negative-fixnum)))
-    (cadr (lazy-nth (position (apply #'max ratings) ratings) (caddr tree)))))
+  (let ((ratings (get-ratings (limit-tree-depth tree *ai-level*) (car tree))))
+    (pick-chance-branch (cadr tree)
+                        (lazy-nth (position (apply #'max ratings) ratings) (caddr tree)))))
 
 (defun play-vs-computer (tree)
   (print-info tree)
   (cond ((lazy-null (caddr tree)) (announce-winner (cadr tree)))
         ((zerop (car tree)) (play-vs-computer (handle-human tree)))
         (t (play-vs-computer (handle-computer tree)))))
+
+(defparameter *dice-odds* #(#(0.84 0.97 1.0  1.0)
+                            #(0.44 0.78 0.94 0.99)
+                            #(0.15 0.45 0.74 0.91)
+                            #(0.04 0.19 0.46 0.72)
+                            #(0.01 0.06 0.22 0.46)))
 
 ; Optimizations: memoization with closures
 
@@ -313,7 +360,7 @@
                                    0.3)))
                          (- yy (* *dice-scale* z 0.8)) col)))
 
-(defparameter *die-colors* '((255 63 63) (63 63 255)))
+(defparameter *die-colors* '((255 63 63) (63 63 255) (63 255 63) (255 63 255)))
 
 (defun draw-board-svg (board chosen-tile legal-tiles)
   (loop for y below *board-size*
@@ -370,16 +417,16 @@
   (fresh-line)
   (let ((w (winners board)))
     (if (> (length w) 1)
-        (format t "The game is a tie between ~a" (mapcar #'player-letter w))
-        (format t "The winner is ~a" (player-letter (car w)))))
+        (format t "The game is a tie between ~a. " (mapcar #'player-letter w))
+        (format t "The winner is ~a. " (player-letter (car w)))))
   (tag a (href "game.html")
-       (princ " play again")))
+       (princ "play again")))
 
 (defun web-handle-human (pos)
   (cond ((not pos) (princ "Please choose a hex to move from:"))
         ((eq pos 'pass)
          (setf *cur-game-tree* (cadr (lazy-car (caddr *cur-game-tree*))))
-         (princ "Your reinforcements have been placed.")
+         (princ "Your reinforcements have been placed. ")
          (tag a (href (make-game-link nil)) (princ "continue")))
         ((not *from-tile*)
          (setf *from-tile* pos)
